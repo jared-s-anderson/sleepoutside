6868ca4f67bcaf4deb424ab38f650d7608c29316
KDabbiero <katelyndabbiero@gmail.com>
2022-01-11 11:08:28 -0700

    commit

--- snowpack.config.js
+++ snowpack.config.js
@@ -2,7 +2,6 @@ module.exports = {
   plugins: [
     // ['@snowpack/plugin-sass' ],
   ],
-<<<<<<< HEAD
   'mount': {
          
     'src': '/'
@@ -21,21 +20,3 @@ module.exports = {
   
 
 }
-=======
-  mount: {
-    src: '/',
-  },
-  buildOptions: {
-    out: 'build',
-    baseUrl: '/sleepoutside/build/',
-    clean: true,
-  },
-  experiments: {
-    optimize: {
-      // 'bundle': true,
-      minify: true,
-      target: 'es2015',
-    },
-  },
-};
->>>>>>> 35315b8541b5599627d089a215ed94766b6733e0
