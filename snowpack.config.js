5b3f04624a855330bd75d2dfe5d7bb8aa143496e
KDabbiero <katelyndabbiero@gmail.com>
2022-01-11 10:55:38 -0700

    Fix config docs

--- snowpack.config.js
+++ snowpack.config.js
@@ -11,12 +11,12 @@ module.exports = {
       'baseUrl': '/sleepoutside/build/',
       'clean': true
    },
-   'experiments': {
+  
     'optimize': {
       // 'bundle': true,
       'minify': true,
       'target': 'es2015'
     }
-  }
+  
 
 }
