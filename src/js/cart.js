0054701c5188ddb8abb6b74230a60f1916c3fd47
Mitchell Salyards <sal17030@byui.edu>
2022-01-13 13:32:27 -0700

    Fixed the add to cart button.

--- src/js/cart.js
+++ src/js/cart.js
@@ -5,9 +5,9 @@ function getLocalStorage(key) {
 function getCartContents() {
   let markup = '';
   const cartItems = getLocalStorage('so-cart');
-  const htmlItems = cartItems.map((item) => renderCartItem(item));
-  document.querySelector('.product-list').innerHTML = htmlItems.join('');
-  // document.querySelector(".product-list").innerHTML = renderCartItem(cartItems);
+  // const htmlItems = cartItems.map((item) => renderCartItem(item));
+  // document.querySelector('.product-list').innerHTML = htmlItems.join('');
+  document.querySelector(".product-list").innerHTML = renderCartItem(cartItems);
 }
 
 function renderCartItem(item) {
