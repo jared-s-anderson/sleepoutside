3ec66af45d3b4089ab17e9efb0c45a2c3a89750c
Mitchell Salyards <sal17030@byui.edu>
2022-01-27 10:33:48 -0700

    Co-authored-by: Jared Anderson <jaredsa-byui@users.noreply.github.com>

---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+import ProductData from './productData.js';
+import ProductList from './productList.js';
+
+const dataSource = new ProductData('tents');
+
+const listElement = document.querySelector('.product-list');
+
+const productList = new ProductList('tents', dataSource, listElement);
+productList.init()
